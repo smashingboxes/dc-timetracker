b851ba7ebcf886dfd52844b74e3f7d646d61e469
AustinLanier <austin@smashingboxes.com>
2018-05-02 16:27:34 -0400

    adds initial timesheet creation test

--- spec/requests/api/v1/timesheets/create_spec.rb
+++ spec/requests/api/v1/timesheets/create_spec.rb
@@ -1,28 +1,31 @@
 require "rails_helper"
 
 describe "POST /api/v1/timesheets", as: :request do
-  subject { post "/api/v1/timesheets", params: params }
+  subject { authenticated_post(path: "/api/v1/timesheets", params: params) }
+
   let(:params) { {} }
   let(:time_entries) do
     (1..6).map do |i|
-        { 
-            date: (Time.zone.now + i.day).midnight.to_s, 
-            hours: i
-        }
+      {
+        date: (Time.zone.now + i.day).midnight.to_s,
+        hours: i
+      }
     end
   end
 
-  context "a user has not submitted a prior request" do
+  context "when a user has not submitted a prior request" do
     let(:params) do
       {
-        activeDate: Time.zone.now.midnight.to_s
-        timeEntriesSets: [
-            { chargeCode: "123abc",
-              timeEntries:  time_entries[0..3]
-            },
-            { chargeCode: "123abc123",
-              timeEntries:  time_entries[3..5]
-            }
+        active_date: Time.zone.now.midnight.to_s,
+        time_entry_sets: [
+          {
+            charge_code: "123abc",
+            time_entries:  time_entries[0..3]
+          },
+          {
+            charge_code: "123abc123",
+            time_entries:  time_entries[3..5]
+          }
         ]
       }
     end
@@ -33,8 +36,9 @@ describe "POST /api/v1/timesheets", as: :request do
     end
 
     pending "creates a timesheet request" do
-      expect { subject }.to change { Timesheet.count }.by(1)
-      expect { subject }.to change { TimeEntrySet.count }.by(2)
-      expect { subject }.to change { TimeEntry.count }.by(6)
+      expect { subject }.to change(Timesheet, :count).by(1)
+      expect { subject }.to change(TimeEntrySet, :count).by(2)
+      expect { subject }.to change(TimeEntry, :count).by(6)
     end
   end
+end
