4b799f1d821049f29d0ca4b5334ea106563c0c6d
AustinLanier <austin@smashingboxes.com>
2018-05-02 17:08:00 -0400

    add timesheet spec, updates user spec

--- spec/models/timesheet_spec.rb
+++ spec/models/timesheet_spec.rb
@@ -1,5 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Timesheet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to respond_to(:period_start) }
+
+  it { is_expected.to belong_to(:user) }
+
+  it { is_expected.to have_many(:time_entry_sets) }
+  it { is_expected.to have_many(:time_entries) }
 end
