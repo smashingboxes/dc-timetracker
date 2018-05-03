60c9eef0c987c53875024fb21e883e43beaa80e2
Derek Kniffin <derek.kniffin@gmail.com>
2018-05-03 10:25:57 -0400

    Add description attribute to charge code

--- app/models/charge_code.rb
+++ app/models/charge_code.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: charge_codes
+#
+#  code        :string
+#  created_at  :datetime         not null
+#  description :text
+#  id          :integer          not null, primary key
+#  updated_at  :datetime         not null
+#
+
 class ChargeCode < ApplicationRecord
 end
--- db/migrate/20180503133336_create_charge_codes.rb
+++ db/migrate/20180503133336_create_charge_codes.rb
@@ -2,6 +2,7 @@ class CreateChargeCodes < ActiveRecord::Migration[5.1]
   def change
     create_table :charge_codes do |t|
       t.string :code
+      t.text :description
       t.timestamps
     end
   end
